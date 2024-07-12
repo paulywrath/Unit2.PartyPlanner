60cf938715071187ed5d97467dfc68cec424cc9d
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-07-12 13:04:36 -0400

    Write pseudocode to render event list

---
+++ index.js
@@ -0,0 +1,11 @@
+// Create state to store fetched data.
+
+// When the page loads, it fetches a list of events from an API and stores it in state. 
+
+// Render a list of events from the data stored in state.
+  // Grab what the list elements will appear inside
+  // Create list element for each event in state
+  // Put event data inside list element
+  // Add them inside parent
+
+// Each event is an object with details about the event. We have the option of using those details later.
